e6f81ce0d5038e1bd40f65e3eb91e0bcde227644
Shawn <ShawnBaumbach@gmail.com>
2019-01-27 02:35:27 -0600

    Mongo List

--- Sample2/codesample/app.js
+++ Sample2/codesample/app.js
@@ -3,21 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs')
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -32,7 +32,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err.message)
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- Sample2/codesample/routes/users.js
+++ Sample2/codesample/routes/users.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-/* GET users listing. */
+// Database
+var db;
+
+MongoClient.connect('mongodb://shawn:Pizza1992@ds259144.mlab.com:59144/mongotest', { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('mongotest'); // Database Name
+  //client.close();
+})
+
+// Load Webpage
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.sendFile('views\\test.html', { root: '.'  });
+  db.collection('quotes').find().toArray((err, results) => {
+    if (err) return console.log(err);
+    console.log(results);
+    // html file (forms) being passed results with the name quotes
+    res.render('forms.ejs', {quotes: results});
+  })
+});
+
+// app.post('/form', (req, res) => {
+//   db.collection('quotes').save(req.body, (err, result) => {
+//     if (err) return console.log(err);
+//     console.log('saved to database');
+//   })
+//     res.redirect('/users');
+// })
+
+router.post('/form', (req, res, next) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log('saved to database');
+  })
+    res.redirect('/users');
+})
+
+// TODO: close not working?
+process.on('SIGINT', function() {
+  MongoClient.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
 });
 
 module.exports = router;
