ab6ac0626c475ea03a920b1c944431cbc3c4a69c
Shawn <ShawnBaumbach@gmail.com>
2019-01-28 05:24:39 -0600

    Better Nav Css

--- Sample2/codesample/public/javascripts/navigation.js
+++ Sample2/codesample/public/javascripts/navigation.js
@@ -1,9 +1,9 @@
 function navbar() {
     var nav = document.getElementById("navigation");
     nav.innerHTML = `
-    <div class="titlenav">
-        <a class="active" href="/">Home</a>
+    <div>
         <div class="topnav">
+            <a href="/">Home</a>
             <a href="/forms">Forms</a>
             <a href="/cats">Cats</a>
         </div>
